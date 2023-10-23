abd17a1c1be6b72b8f3d3a74e6fc22f40193be9d
HatimTM <43782716+HatimTM@users.noreply.github.com>
2023-10-23 16:23:49 +0300

    Update Extract-part-of-sequence-in-FASTA-format.py

--- Extract-part-of-sequence-in-FASTA-format.py
+++ Extract-part-of-sequence-in-FASTA-format.py
@@ -4,6 +4,6 @@ dnaread = mydoc.read()
 
 seqRange = dnaread[6052:7916]
 
-cifseqopen = open("CREATE_EMPTY_FILE.fasta","w")
+myseqopen = open("CREATE_EMPTY_FILE.fasta","w")
 
-cifseqopen.write(seqRange)
+myseqopen.write(seqRange)
